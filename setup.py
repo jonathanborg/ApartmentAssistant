1595e02452739a3e22be712061c2191647aaf921
Jonathan Borg <jonathanborg1996@gmail.com>
2024-01-19 15:15:59 +0100

    Created Initial Project Setup (Cookie Cutter)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository to help find information about possible apartments, locations and their respective pricing',
+    author='Jonathan Borg',
+    license='',
+)
